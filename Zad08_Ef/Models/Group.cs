aa95d7dcb0f351a0afe9696bc037710c27a95ae4
anteklichy <antek.lichy@gmail.com>
2024-05-20 13:54:20 +0200

    Initial commit

---
+++ Zad08_Ef/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zad08_Ef.Models;
+
+public partial class Group
+{
+    public int IdGroup { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<StudentGroup> StudentGroups { get; set; } = new List<StudentGroup>();
+}
